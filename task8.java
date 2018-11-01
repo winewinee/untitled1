01c0d514d3c5b23fac424987486116bc3501c13b
Vitalii <44645036+winewinee@users.noreply.github.com>
2018-11-01 14:02:00 +0200

    Add files via upload

--- task8.java
+++ task8.java
@@ -1,4 +1,4 @@
-package Test.lesson5;
+package lesson5;
 //Для заданных функций найдите их максимальное значение и
 //x, при котором оно достигается, на указанных интервалах с
 //шагом 0.01 с использованием цикла/циклов for:
@@ -7,11 +7,13 @@ package Test.lesson5;
 //8) e^x на интервале [-10, 10] Math.exp(x-2)
 public class task8 {
     public static void main(String[] args) {
-        double max = 10;
+        double max = 0;
         double e = Math.E;
-        //double function = Math.pow(e, x);
-        for (double x = -10; x <= 10 ; x += 0.01) {
-            double function = Math.pow(e, x);
+        double function = Math.pow(e, max);
+
+        for (double x = 0; x <= 10 ; x += 0.01) {
+            function = Math.pow(e, x);
+
             if (function > Math.pow(e, max)) {
                 max = x;
                 function = Math.pow(e, x);
